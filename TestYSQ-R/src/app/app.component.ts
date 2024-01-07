d1ca1f5c8ae21648d9e6c68989809218ddf31098
kanekochi <kazup930@gmail.com>
2024-01-07 12:46:31 +0900

    update:配列から問題と回答を作成。その合計を取得する処理を追加。

--- TestYSQ-R/src/app/app.component.ts
+++ TestYSQ-R/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatRadioModule} from '@angular/material/radio';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -17,11 +17,24 @@ import { ReactiveFormsModule } from '@angular/forms';
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  title = 'TestYSQ-R';
-  score :  FormControl = new FormControl<number>(0);
-  scoreText:FormControl = new FormControl<number|null>(1111);
+  statementsGroup:FormGroup = new FormGroup({});
+  score = new FormControl<number>(0);
+  scoreText = new FormControl<number|null>(null);
+  problemStatements = ["テスト１","テスト２","テスト３","テスト４"]
 
   public getScore(){
-    this.scoreText.setValue(this.score.value)
+    var totaling = 0;
+    this.problemStatements.forEach((statment,index) =>{
+      totaling += +this.statementsGroup.get("score_"+index)?.value
+    }
+    )
+    this.scoreText.setValue(totaling);
+  }
+
+  public ngOnInit(){
+    this.problemStatements.forEach((statment,index) =>{
+      this.statementsGroup.addControl("score_"+index,new FormControl<number>(0))
+    }
+    )
   }
 }

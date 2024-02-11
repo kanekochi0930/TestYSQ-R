8011ea25daa0bd18de338e3bd5300ec26d0f58f6
kanekochi <kazup930@gmail.com>
2024-02-11 17:20:31 +0900

    update:点数を表形式で出力できるように修正。

--- TestYSQ-R/src/app/app.component.ts
+++ TestYSQ-R/src/app/app.component.ts
@@ -6,38 +6,48 @@ import { FormControl, FormGroup } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProblemStatement } from './interface/problem-statement';
 import { YsqrProblemStatements } from './const/ysqrProblemStatement';
+import { ProblemTotalingScore } from './interface/ploblem-totaling-score';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatRadioModule,MatButtonModule,MatInputModule,MatFormFieldModule,ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet,MatRadioModule,MatButtonModule,MatInputModule,MatFormFieldModule,MatTableModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
+  
   statementsGroup:FormGroup = new FormGroup({});
   score = new FormControl<number>(0);
   scoreText = new FormControl<number|null>(null);
   problemStatements:ProblemStatement[] = YsqrProblemStatements;
+  problemTotalingScores:ProblemTotalingScore[] =[];
+  tableDataSorce:MatTableDataSource<ProblemTotalingScore>= new MatTableDataSource<ProblemTotalingScore>;
+
+  displayedColumns: string[] = ['blockTitele', 'totaling'];
   
   public getScore(){
-    var totaling = 0;
-    this.problemStatements.forEach((blockTitle) =>{
-      blockTitle.problemStatement.forEach((_,index)=>{
-        totaling += +this.statementsGroup.get("score_"+blockTitle.blockTitele+"_"+index)?.value
+    this.problemStatements.forEach((problem) =>{
+      var totaling = 0;
+      problem.problemStatement.forEach((_,index)=>{
+        totaling += +this.statementsGroup.get("score_"+problem.blockTitele+"_"+index)?.value
       })
+      const scoreData :ProblemTotalingScore ={ blockTitele:problem.blockTitele , totaling:totaling}
+      this.problemTotalingScores.push(scoreData)
     })
-    this.scoreText.setValue(totaling);
+    this.tableDataSorce = new MatTableDataSource<ProblemTotalingScore>(this.problemTotalingScores.sort((a,b) => b.totaling-a.totaling))
+    this.problemTotalingScores= [];
   }
 
   public ngOnInit(){
-    this.problemStatements.forEach((blockTitle) =>{
-      blockTitle.problemStatement.forEach((_,index)=>{
-      this.statementsGroup.addControl("score_"+blockTitle.blockTitele+"_"+index,new FormControl<number>(0))
+    this.problemStatements.forEach((problem) =>{
+      problem.problemStatement.forEach((_,index)=>{
+      this.statementsGroup.addControl("score_"+problem.blockTitele+"_"+index,new FormControl<number>(0))
       })
     })
   }
---
+++ TestYSQ-R/src/app/interface/ploblem-totaling-score.ts
@@ -0,0 +1,4 @@
+export interface ProblemTotalingScore {
+    blockTitele:string
+    totaling:number
+}

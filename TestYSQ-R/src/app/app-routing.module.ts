79ea006067244b7a915dc9e54fa08894c8c89e12
kanekochi <kazup930@gmail.com>
2024-03-02 16:35:01 +0900

    update:スタンドアローンアプリの場合の記述に修正。画面遷移ができるようになった。

--- TestYSQ-R/src/app/app-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { routes } from './app.routes';
-import { RouterModule } from '@angular/router';
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- TestYSQ-R/src/app/app.component.ts
+++ TestYSQ-R/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],

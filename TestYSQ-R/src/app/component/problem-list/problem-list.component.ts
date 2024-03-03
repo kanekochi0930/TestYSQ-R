c3c77577b46f95697e0470f51708cb60d5ce9161
kanekochi <kazup930@gmail.com>
2024-03-03 18:19:23 +0900

    update:Urlクエリに診断結果を入れて、resultページで表示できるように修正。

--- TestYSQ-R/src/app/component/problem-list/problem-list.component.ts
+++ TestYSQ-R/src/app/component/problem-list/problem-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { YsqrProblemStatements } from '../../const/ysqrProblemStatement';
-import { ProblemAverageScore } from '../../interface/ploblem-average-score';
+import { ProblemAverageScoreQuery as ProblemAverageScoreUrlQuery } from '../../interface/ploblem-average-score';
 import { ProblemStatement } from '../../interface/problem-statement';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -17,41 +17,35 @@ import { Router } from '@angular/router';
 })
 export class ProblemListComponent {
   statementsGroup: FormGroup = new FormGroup({});
-  score = new FormControl<number>(0);
-  scoreText = new FormControl<number | null>(null);
   problemStatements: ProblemStatement[] = YsqrProblemStatements;
-  problemAverageScores: ProblemAverageScore[] = [];
   notAnswereds: string[] = [];
 
   constructor(private router: Router) {}
 
   public getScore() {
-    this.router.navigate(['/result']);
-    // this.problemAverageScores = [];
-    // this.notAnswereds = [];
-    // // 問題毎に、合計を計算
-    // this.problemStatements.forEach((problem) => {
-    //   var totaling = 0;
-    //   problem.problemStatement.forEach((_, index) => {
-    //     var score = +this.statementsGroup.get('score_' + problem.blockTitele + '_' + index)?.value;
-    //     if (score == 0) {
-    //       this.notAnswereds.push(problem.blockTitele + 'のNo.' + (index + 1) + 'が未回答です。');
-    //     }
-    //     totaling += score;
-    //   });
-    //   var average = totaling / problem.problemStatement.length;
-    //   const scoreData: ProblemAverageScore = {
-    //     blockTitele: problem.blockTitele,
-    //     average: average,
-    //   };
-    //   this.problemAverageScores.push(scoreData);
-    // });
+    this.notAnswereds = [];
+    const problemAverageScoreUrlQuery: ProblemAverageScoreUrlQuery = {};
+    // 問題毎に、平均を計算しクエリパラメータに設定し遷移。
+    this.problemStatements.forEach((problem) => {
+      var totaling = 0;
+      problem.problemStatement.forEach((_, index) => {
+        var score = +this.statementsGroup.get('score_' + problem.blockTitele + '_' + index)?.value;
+        if (score == 0) {
+          this.notAnswereds.push(problem.blockTitele + 'のNo.' + (index + 1) + 'が未回答です。');
+        }
+        totaling += score;
+      });
+      var average = totaling / problem.problemStatement.length;
+      problemAverageScoreUrlQuery[problem.blockTitele] = average;
+    });
     // ここキモいから直したい。
     // if (!this.notAnswereds.length) {
     //   this.tableDataSorce = new MatTableDataSource<ProblemAverageScore>(
     //     this.problemAverageScores.sort((a, b) => b.average - a.average)
     //   );
     // }
+
+    this.router.navigate(['/result'], { queryParams: problemAverageScoreUrlQuery });
   }
 
   public ngOnInit() {
--- TestYSQ-R/src/app/page/problem-result/problem-result.component.ts
+++ TestYSQ-R/src/app/page/problem-result/problem-result.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ProblemAverageScore } from '../../interface/ploblem-average-score';
+import { YsqrProblemStatements } from '../../const/ysqrProblemStatement';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-problem-result',
@@ -14,4 +16,17 @@ export class ProblemResultComponent {
   tableDataSorce: MatTableDataSource<ProblemAverageScore> =
     new MatTableDataSource<ProblemAverageScore>();
   displayedColumns: string[] = ['blockTitele', 'average'];
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    YsqrProblemStatements.forEach((problem) => {
+      var average = 0;
+      this.route.queryParams.subscribe((params) => {
+        average = params[problem.blockTitele];
+      });
+      this.tableDataSorce.data.push({ blockTitele: problem.blockTitele, average: average });
+    });
+    this.tableDataSorce.data.sort((a, b) => b.average - a.average);
+  }
 }

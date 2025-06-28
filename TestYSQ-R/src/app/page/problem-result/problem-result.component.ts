9fdae8c7af69fb0c5aa1c970857097dd15085c8e
kanekochi <kazup930@gmail.com>
2025-06-28 20:39:34 +0900

    Update:回答結果のデザインを修正

--- TestYSQ-R/src/app/page/problem-result/problem-result.component.ts
+++ TestYSQ-R/src/app/page/problem-result/problem-result.component.ts
@@ -1,32 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ProblemAverageScore } from '../../interface/problem-average-score';
-import { YsqrProblemStatements } from '../../const/ysqrProblemStatement';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-problem-result',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+  ],
   templateUrl: './problem-result.component.html',
   styleUrl: './problem-result.component.scss',
 })
-export class ProblemResultComponent {
-  tableDataSorce: MatTableDataSource<ProblemAverageScore> =
-    new MatTableDataSource<ProblemAverageScore>();
-  displayedColumns: string[] = ['blockTitele', 'average'];
+export class ProblemResultComponent implements OnInit {
+  results: ProblemAverageScore[] = [];
+  readonly maxScore = 6; // Max score for a single question
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    YsqrProblemStatements.forEach((problem) => {
-      var average = 0;
-      this.route.queryParams.subscribe((params) => {
-        average = params[problem.blockTitele];
-      });
-      this.tableDataSorce.data.push({ blockTitele: problem.blockTitele, average: average });
+    this.route.queryParams.subscribe((params) => {
+      this.results = Object.keys(params).map((key) => ({
+        blockTitele: key,
+        average: parseFloat(params[key]),
+      }));
+      // Sort results by average score in descending order
+      this.results.sort((a, b) => b.average - a.average);
     });
-    this.tableDataSorce.data.sort((a, b) => b.average - a.average);
+  }
+
+  retakeTest() {
+    this.router.navigate(['/']);
   }
 }
